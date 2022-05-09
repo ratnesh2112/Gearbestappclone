72956874550fa8ffb3c608ef17a3a5414084578a
Sahilkumar26244 <kumarsahil8981@gmail.com>
2022-05-09 11:30:06 +0530

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,249 @@
+let ringsData = [
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6907043732781199360/16492/goods_thumb_220-v2/eb108f2fda03.jpg",
+        name: "New A2000 Mini Projector 320x240 Pixels 800 Lumens..." ,
+        price:"$28.00",
+        strikedoffprice:"$42.93"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6615272824267153408/16421/goods_thumb_220-v4/a0da751fd8f8.jpg",
+        name: "Original Xiaomi Redmi Airdots 2 TWS Fone Bluetooth Earphones..." ,
+        price:"$201.10",
+        strikedoffprice:"$67.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16414/goods_thumb_220-v1/e1c76b337a79.jpg",
+        name: "Original Xiaomi Mi Band 6 / 6 NFC Smart Wristband 1.56 inch..." ,
+        price:"$1629.00",
+        strikedoffprice:"$8.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6899301501118902272/16463/goods_thumb_220-v4/ad2a20a94177.jpg",
+        name: "Luminous Series One-Cable Three Purpose USB TO MTL..." ,
+        price:"$38.99",
+        strikedoffprice:"$38.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6771730315905003520/16419/goods_thumb_220-v1/a2fbd39cb9c5.jpg",
+        name: "original Xiaomi Mi Robot Vacuum Cleaner  Main Brushes..." ,
+        price:"$17.69",
+        strikedoffprice:"$7.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878388619120406528/16464/goods_thumb_220-v2/134ae8309154.jpg",
+        name: "500X25 50000M Portable Zoom HD Professional Powerful..." ,
+        price:"$16.33",
+        strikedoffprice:"$14.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16434/goods_thumb_220-v1/330e9a95381b.jpg",
+        name: "Car Radio 7 inch HD Autoradio Multimedia Player 2DIN..." ,
+        price:"$89.00",
+        strikedoffprice:"$29.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16475/goods_thumb_220-v1/74b8450aa656.jpg",
+        name: "Andon Portable Micro-atomizer Nebulizer Mini Handheld..." ,
+        price:"$39.88",
+        strikedoffprice:"$53.64"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15892/goods_thumb_220-v1/a8a30832f5e9.jpg",
+        name: "UMIDIGI A11 Tab 10.4  4GB 128GB 8000mAh Octa Core..." ,
+        price:"$22.99",
+        strikedoffprice:"$42.93"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6883469851634102272/16497/goods_thumb_220-v1/8b6485baad75.jpg",
+        name: "L09 Gimbal Stabilizer With Fill Light Wireless Bluetooth..." ,
+        price:"$62.99",
+        strikedoffprice:"$2.59"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16499/goods_thumb_220-v5/6233d35f6f8a.jpg",
+        name: "Electric Water Kettle 34 Oz Stainless Steel - Light Green" ,
+        price:"$21.99",
+        strikedoffprice:"$16.90"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/126354806df6.jpg",
+        name: "Global Version Xiaomi Redmi Note 9 3GB RAM 64GB 128GB..." ,
+        price:"$119.99",
+        strikedoffprice:"$8.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878024886355423232/16416/goods_thumb_220-v1/9dbf47dbe530.jpg",
+        name: "Xiaomi Mijia Automatic Folding Umbrella Sunny Rainy..." ,
+        price:"$219.00",
+        strikedoffprice:"$114.30"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16488/goods_thumb_220-v1/d238ad6a3d2a.jpg",
+        name: "Mijia Sonic Electric Toothbrush T100 Adult Ultrasonic..." ,
+        price:"$15.99",
+        strikedoffprice:"$35.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6615272824267153408/16421/goods_thumb_220-v3/4b0ffdc40949.jpg",
+        name: "Global Version Wanbo Projector T6 Max LED 550 ANSI Smart..." ,
+        price:"$33.50",
+        strikedoffprice:"$114.00"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16414/goods_thumb_220-v1/b1589d2d9db3.jpg",
+        name: "RC Spinosaurus 2CH 2.4G Radio Control Dinosaur Dragon Pet..." ,
+        price:"$12.00",
+        strikedoffprice:"$170.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16472/goods_thumb_220-v1/3a6f1ce7b307.jpg",
+        name: "Brew Adjustable Temperature Kettle Electric Clear - Multi" ,
+        price:"$68.99",
+        strikedoffprice:"$13.59"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/16121/goods_thumb_220-v9/ca5568a2f175.jpg",
+        name: "Xiaomi Mi Piston 3 Type C Earphone USB-C in Ear Earbuds..." ,
+        price:"$119.99",
+        strikedoffprice:"$99.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16420/goods_thumb_220-v1/feb032eccd26.jpg",
+        name: "Mijia E27 Smart LED Bulb Bluetooth MESH Version 5W..." ,
+        price:"$779.00",
+        strikedoffprice:"$329.00"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16416/goods_thumb_220-v1/e540b94c9c89.jpg",
+        name: "Xiaomi Mijia Bluetooth Temperature Humidity..." ,
+        price:"$119.99",
+        strikedoffprice:"$8.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6899301501118902272/16463/goods_thumb_220-v5/2f380d04f8f3.jpg",
+        name: "Original Xiaomi Mi Airdots Air 2 SE TWS Bluetooth Earphone..." ,
+        price:"$21.00",
+        strikedoffprice:"$22.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6882689559126274048/16489/goods_thumb_220-v7/4e06e8fb9369.jpg",
+        name: "BT-S2 motorcycle snowmobile helmet bluetooth headset with..." ,
+        price:"$125.00",
+        strikedoffprice:"$16.39"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16465/goods_thumb_220-v3/c756d2c80fe9.jpg",
+        name: "Xiaomi Redmi AirDots 2 Wireless Earphone Bluetooth..." ,
+        price:"$455.00",
+        strikedoffprice:"$25.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16486/goods_thumb_220-v1/965097e8a620.jpg",
+        name: "Aqara Smart Temperature Humidity Sensor Zigbee..." ,
+        price:"$22.99",
+        strikedoffprice:"$10.69"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6899301501118902272/16510/goods_thumb_220-v7/5c6925b46bdf.jpg",
+        name: "Xiaomi Mijia Hair Clipper 2200mAh Men Women..." ,
+        price:"$89.00",
+        strikedoffprice:"$36.58"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6883820374342168576/16499/goods_thumb_220-v1/2177d4dd3ef7.jpg",
+        name: "TOCMOC T200 200W Handheld Wireless Vacuum Cleaner..." ,
+        price:"$39,98",
+        strikedoffprice:"$409.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16456/goods_thumb_220-v1/5ef9d5b1a9a0.jpg",
+        name: "Ulefone Power Armor 13 Global Version 13200mAh Battery 8GB..." ,
+        price:"$22.00",
+        strikedoffprice:"$37.88"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6899301501118902272/16498/goods_thumb_220-v13/c0d56aec4c65.jpg",
+        name: "Original Xiaomi Redmi Buds 3 Lite Youth Edition Earphone..." ,
+        price:"$16.33",
+        strikedoffprice:"$17.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15927/goods_thumb_220-v1/3bdf2c9be242.jpg",
+        name: "Main Brush Filters Cleaner Side Brushes Accessories for XIAOMI..." ,
+        price:"$17.89",
+        strikedoffprice:"$160.00"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16465/goods_thumb_220-v1/7d8d73e20fc4.jpg",
+        name: "Original Xiaomi Redmi Buds 3 Lite Youth Edition Earphone..." ,
+        price:"$22.99",
+        strikedoffprice:"$28.98"
+
+    },
+
+]
+
+window.addEventListener("load",function(){
+    displayData(ringsData)
+});
+
+function displayData(ringsData) {
+
+    ringsData.map(function (elem) {
+    var box = document.createElement("div");
+
+    var img = document.createElement("img");
+    img.src = elem.image_url;
+
+    var name = document.createElement("p");
+    name.textContent = elem.name;
+    name.setAttribute("id","name1")
+    
+
+    var box2 = document.createElement("div");
+    box2.setAttribute("id","priceBox")
+
+    var price = document.createElement("p");
+    price.innerText = elem.price;
+    price.setAttribute("id","price-style")
+
+    var strickedPrice = document.createElement("p");
+    strickedPrice.innerText=elem.strikedoffprice;
+    strickedPrice.setAttribute("id","stricked")
+
+    box2.append(price,strickedPrice)
+
+    box.append(img, name,box2);
+
+    document.querySelector("#container1").append(box);
+  });
+    }
---
+++ Scripts/new.js
@@ -0,0 +1,342 @@
+
+let ringsData = [
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878030535709618176/16512/goods_thumb_220-v1/15cfc96f41ad.jpg",
+        name: "New A2000 Mini Projector 320x240 Pixels 800 Lumens..." ,
+        price:"$28.00",
+        strikedoffprice:"$129.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6878025592978206720/16513/goods_thumb_220-v1/cf8588a8784a.jpg",
+        name: "H4 Smart Gimbal Stabilizer 3-Axis Bluetooth Extendable..." ,
+        price:"$201.10",
+        strikedoffprice:"$47.11"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878030535709618176/16508/goods_thumb_220-v1/ff08fa124b71.jpg",
+        name: "Outdoor Portable Type-C Mini Electric Shaver Long-lasting..." ,
+        price:"$1629.00",
+        strikedoffprice:"R$45.95"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6911257965974712320/16499/goods_thumb_220-v1/86d805e3f5bc.jpg",
+        name: "Electric Gooseneck Kettle Hot Water Kettle 100% Stainless..." ,
+        price:"$38.99",
+        strikedoffprice:"$179.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6507287161137004544/16511/goods_thumb_220-v3/4c3002e5cb14.jpg",
+        name: "Global Version Xiaomi Redmi Note 9T 5G 4GB 64GB..." ,
+        price:"$17.69",
+        strikedoffprice:"$206.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6866331803104505856/16503/goods_thumb_220-v4/3d1ade23b71d.jpg",
+        name: "LEEPEE 14MM M22 Socket 1/4&quot Auto Quick Release..." ,
+        price:"$16.33",
+        strikedoffprice:"$129.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6907992381598134272/16512/goods_thumb_220-v1/1d5b21329ed7.jpg",
+        name: "Disinfecting Soap Paper Bath Soap Flakes Mini Cleaning..." ,
+        price:"$89.00",
+        strikedoffprice:"$39.92"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878400698325856256/16442/goods_thumb_220-v1/cce9fb87d827.jpg",
+        name: "Andon Portable Micro-atomizer Nebulizer Mini Handheld..." ,
+        price:"$39.88",
+        strikedoffprice:"$133.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6907992381598134272/16512/goods_thumb_220-v1/95652d9d4433.jpg",
+        name: "UMIDIGI A11 Tab 10.4  4GB 128GB 8000mAh Octa Core..." ,
+        price:"$22.99",
+        strikedoffprice:"$264.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6878025592978206720/16513/goods_thumb_220-v3/cd850992fb3d.jpg",
+        name: "L09 Gimbal Stabilizer With Fill Light Wireless Bluetooth..." ,
+        price:"$62.99",
+        strikedoffprice:"$129.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6911257965974712320/16496/goods_thumb_220-v1/e72ab33a830b.jpg",
+        name: "Electric Water Kettle 34 Oz Stainless Steel - Light Green" ,
+        price:"$21.99",
+        strikedoffprice:"$377.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878400698325856256/16510/goods_thumb_220-v1/34e409124ee3.jpg",
+        name: "Global Version Xiaomi Redmi Note 9 3GB RAM 64GB 128GB..." ,
+        price:"$119.99",
+        strikedoffprice:"$172.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878400698325856256/16510/goods_thumb_220-v1/6737ca9064e7.jpg",
+        name: "Xiaomi Mijia Automatic Folding Umbrella Sunny Rainy..." ,
+        price:"$219.00",
+        strikedoffprice:"$168.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6507287161137004544/16510/goods_thumb_220-v3/ddf0d3cb8167.jpg",
+        name: "Mijia Sonic Electric Toothbrush T100 Adult Ultrasonic..." ,
+        price:"$15.99",
+        strikedoffprice:"$119.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6878025592978206720/16511/goods_thumb_220-v1/95a1f339f6bb.jpg",
+        name: "Global Version Wanbo Projector T6 Max LED 550 ANSI Smart..." ,
+        price:"$33.50",
+        strikedoffprice:"$108.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/desc/6911257965974712320/16496/goods_thumb_220-v1/ba69e59daff1.jpg",
+        name: "RC Spinosaurus 2CH 2.4G Radio Control Dinosaur Dragon Pet..." ,
+        price:"$12.00",
+        strikedoffprice:"$4.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6907992381598134272/16507/goods_thumb_220-v1/55116d8399d2.jpg",
+        name: "Brew Adjustable Temperature Kettle Electric Clear - Multi" ,
+        price:"$68.99",
+        strikedoffprice:"$229.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6866331803104505856/16508/goods_thumb_220-v3/44493c2742dd.jpg",
+        name: "3 in 1 USB Charger Cable For Mobile Phone Micro USB Type..." ,
+        price:"$119.99",
+        strikedoffprice:"$6.60"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878400698325856256/16510/goods_thumb_220-v1/3dcc77d42bab.jpg",
+        name: "MJX Bugs B16 PRO GPS Drone with 4K HD Camera 28mins..." ,
+        price:"$779.00",
+        strikedoffprice:"$100.59"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6507287161137004544/16510/goods_thumb_220-v3/4c5ded598090.jpg",
+        name: "Kettle Temperature Control Pour Over Electric Kettle for..." ,
+        price:"$119.99",
+        strikedoffprice:"$48.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6878025592978206720/16511/goods_thumb_220-v1/49107b42281b.jpg",
+        name: "F185 PRO Mini RC Drone with 4K Dual Camera WIFI FPV Aerial..." ,
+        price:"$21.00",
+        strikedoffprice:"$26.30"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6911257965974712320/16493/goods_thumb_220-v1/31274c094d3b.jpg",
+        name: "Classic Harmonica Whistling Teakettle 1.8 quart Glacier Blue..." ,
+        price:"$125.00",
+        strikedoffprice:"$220.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6907992381598134272/16508/goods_thumb_220-v1/43d751fe4e00.jpg",
+        name: "Global Version Xiaomi Redmi Note 11 Smartphone..." ,
+        price:"$455.00",
+        strikedoffprice:"$36.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6866331803104505856/16508/goods_thumb_220-v3/48c3d3d483ce.jpg",
+        name: "Pet Measuring Cup Cat Food Dog Food Electronic Scale..." ,
+        price:"$22.99",
+        strikedoffprice:"$26.80"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878400698325856256/16510/goods_thumb_220-v1/37cb11b523cf.jpg",
+        name: "Electronic Spoon Scale Spoon Weighing Food Scale 500g/0.1g..." ,
+        price:"$89.00",
+        strikedoffprice:"$52.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6507287161137004544/16510/goods_thumb_220-v3/ecbf55c03340.jpg",
+        name: "Salange P88 1080P Projector 300 inch Screen Full HD Home..." ,
+        price:"$39,98",
+        strikedoffprice:"$235.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6911257965974712320/16493/goods_thumb_220-v1/cec0ea0b6060.jpg",
+        name: "Kettle Temperature Control Pour Over Electric Kettle for..." ,
+        price:"$22.00",
+        strikedoffprice:"$30.80"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6907992381598134272/16512/goods_thumb_220-v1/82547b2fd49d.jpg",
+        name: "UMIDIGI BISON GT2 PRO Android 12 IP68 Rugged..." ,
+        price:"$16.33",
+        strikedoffprice:"$44.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/store/6866331803104505856/16508/goods_thumb_220-v3/6f2789db035b.jpg",
+        name: "FEIYUE FY-07 FY07 RC Car 1/12 4WD 2.4G 45KM/h High Speed..." ,
+        price:"$17.89",
+        strikedoffprice:"$279.99"
+
+    },
+    {
+        image_url:"https://gloimg.gbtcdn.com/soa/gb/item/6878400698325856256/16510/goods_thumb_220-v1/2d884ce1b74b.jpg",
+        name: "Disinfecting Soap Paper Bath Soap Flakes Mini Cleaning..." ,
+        price:"$22.99",
+        strikedoffprice:"$896.69"
+
+    },
+
+]
+
+window.addEventListener("load",function(){
+    displayData(ringsData)
+});
+
+function displayData(ringsData) {
+
+    ringsData.map(function (elem) {
+    var box = document.createElement("div");
+
+    var img = document.createElement("img");
+    img.src = elem.image_url;
+
+    var name = document.createElement("p");
+    name.textContent = elem.name;
+    name.setAttribute("id","name1")
+    
+
+    var box2 = document.createElement("div");
+    box2.setAttribute("id","priceBox")
+
+    var price = document.createElement("p");
+    price.innerText = elem.price;
+    price.setAttribute("id","price-style")
+
+    var strickedPrice = document.createElement("p");
+    strickedPrice.innerText=elem.strikedoffprice;
+    strickedPrice.setAttribute("id","stricked")
+
+    box2.append(price)
+
+    box.append(img, name,box2);
+
+    document.querySelector("#container1").append(box);
+  });
+    }
+
+// sliders
+
+function responsiveSlider() {
+    const slider = document.querySelector('.container');
+    let sliderWidth = slider.offsetWidth / 3;
+    const sliderList = document.querySelector('ul');
+    let items = sliderList.querySelectorAll('li').length -2 ;
+    let count = 1;
+    
+    window.addEventListener('resize', function() {
+      sliderWidth = slider.offsetWidth;
+    });
+    
+    function prevSlide() {
+      if(count > 1) {
+        count = count - 2;
+        sliderList.style.left = '-' + count * sliderWidth + 'px';
+        count++;
+      }else if(count == 1) {
+        count = items - 1;
+        sliderList.style.left = '-' + count * sliderWidth + 'px';
+        count++;
+      }
+      
+    }
+    function nextSlide() {
+      if(count < items) {
+        sliderList.style.left = '-' + count * sliderWidth + 'px';
+        count++;
+        
+      }else if(count == items) {
+        sliderList.style.left = '0px';
+        count = 1;
+        
+      }
+    }
+    prev.addEventListener('click', prevSlide);
+    next.addEventListener('click', nextSlide);
+    setInterval(function() {
+      nextSlide();
+    }, 4000);
+  }
+  
+  window.onload = function() {
+    responsiveSlider();
+  }
+
+
+// slider2
+
+let btn1=document.getElementById("pro-prev").addEventListener("click",prevfun)
+    let btn2=document.getElementById("pro-next").addEventListener("click",Nextfun)
+    let Next=true;
+    let Prev=false;
+
+
+
+    function prevfun(){
+        if(Prev){
+            document.getElementsByClassName("item").innerText="";
+
+            document.getElementById("item1").innerText="All";
+            document.getElementById("item2").innerText="Consumer Electronics";
+            document.getElementById("item3").innerText="Lighting";
+            document.getElementById("item4").innerText="Appliances";
+            document.getElementById("item5").innerText="Smartphones";
+            document.getElementById("item6").innerText="Computers & Tablets";
+            document.getElementById("item7").innerText="3D Printer & Supplies";
+            document.getElementById("item8").innerText="Home & Garden";
+            Next=true;
+            Prev=false;
+        }
+    }
+
+    function Nextfun(){
+        if(Next){
+            document.getElementsByClassName("item").innerText="";
+
+                document.getElementById("item1").innerText="Outdoors & Sports";
+                document.getElementById("item2").innerText="Shoes & Apparel";
+                document.getElementById("item3").innerText="Motor & Car Electronics";
+                document.getElementById("item4").innerText="Phones Accessories";
+                document.getElementById("item5").innerText="Toys & Kids";
+                document.getElementById("item6").innerText="Beauty & Care";
+                document.getElementById("item7").innerText="TV Box";
+                document.getElementById("item8").innerText="Watches & Jewelry";         
+                Prev=true;
+                Next=false;
+            }
+    }
+
